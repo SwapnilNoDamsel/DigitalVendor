869f717e9b4341e794a609cc2dfdc3e7387ed4e1
SwapnilNoDamsel <swapnilsawal1910@gmail.com>
2026-09-13 21:02:25 +0530

    Configure Aiven MySQL SSL

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,14 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "digitalvendor",
+
+  ssl: process.env.DB_SSL_CA
+    ? {
+        ca: process.env.DB_SSL_CA.replace(/\\n/g, "\n"),
+        rejectUnauthorized: true
+      }
+    : undefined,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
